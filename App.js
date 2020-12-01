a9ab1498e99e52b28a0597c6fcd2fe6018eb466a
adprm <aditiyaprmn00@gmail.com>
2020-12-01 10:27:13 +0700

    botton navigation icon

--- App.js
+++ App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={{flex: 1}}>
-      {/* <Text>Open up App.js to start working on your app!</Text> */}
-      <View style={{flex: 1, backgroundColor: 'blue'}}></View>
-      <View style={{backgroundColor: 'yellow', height: 54, flexDirection: 'row'}}>
-        <View style={{backgroundColor: 'red', flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-          <Text>Home</Text>
+      {/* Content */}
+      <View style={{flex: 1, backgroundColor: 'pink'}}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+
+      {/* Navigation */}
+      <View style={{height: 54, flexDirection: 'row', backgroundColor: 'white'}}>
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+          <Image style={{width: 26, height: 26}} source={require('./icon/home-active.png')} />
+          <Text style={{fontSize: 10, color: '#545454', marginTop: 4, color: '#43ab4a'}}>Home</Text>
         </View>
-        <View style={{backgroundColor: 'green', flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-          <Text>Orders</Text>
-          </View>
-        <View style={{backgroundColor: 'white', flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-          <Text>Help</Text>
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+          <Image style={{width: 26, height: 26}} source={require('./icon/order.png')} />
+          <Text style={{fontSize: 10, color: '#545454', marginTop: 4}}>Orders</Text>
         </View>
-        <View style={{backgroundColor: 'pink', flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-          <Text>Inbox</Text>
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+          <Image style={{width: 26, height: 26}} source={require('./icon/help.png')} />
+          <Text style={{fontSize: 10, color: '#545454', marginTop: 4}}>Help</Text>
         </View>
-        <View style={{backgroundColor: 'brown', flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-          <Text>Account</Text>
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+          <Image style={{width: 26, height: 26}} source={require('./icon/inbox.png')} />
+          <Text style={{fontSize: 10, color: '#545454', marginTop: 4}}>Inbox</Text>
+        </View>
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+          <Image style={{width: 26, height: 26}} source={require('./icon/account.png')} />
+          <Text style={{fontSize: 10, color: '#545454', marginTop: 4}}>Account</Text>
         </View>
       </View>
     </View>
