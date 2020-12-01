39ab6ac6a25173209cc1ab2bd684d7f60c71f489
adprm <aditiyaprmn00@gmail.com>
2020-12-01 10:01:43 +0700

    bottom navigation position

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{flex: 1}}>
+      {/* <Text>Open up App.js to start working on your app!</Text> */}
+      <View style={{flex: 1, backgroundColor: 'blue'}}></View>
+      <View style={{backgroundColor: 'yellow', height: 54, flexDirection: 'row'}}>
+        <View style={{backgroundColor: 'red', flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+          <Text>Home</Text>
+        </View>
+        <View style={{backgroundColor: 'green', flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+          <Text>Orders</Text>
+          </View>
+        <View style={{backgroundColor: 'white', flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+          <Text>Help</Text>
+        </View>
+        <View style={{backgroundColor: 'pink', flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+          <Text>Inbox</Text>
+        </View>
+        <View style={{backgroundColor: 'brown', flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+          <Text>Account</Text>
+        </View>
+      </View>
     </View>
   );
 }
