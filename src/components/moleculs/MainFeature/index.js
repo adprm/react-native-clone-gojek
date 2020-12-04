471350c0c9bea64c7e8ef525da6009d0c6562a61
adprm <aditiyaprmn00@gmail.com>
2020-12-04 14:11:55 +0700

    Main feature component

---
+++ src/components/moleculs/MainFeature/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+const MainFeature = (props) => {
+    return (
+        <View style={{width: '25%', alignItems: 'center', marginBottom: 18}}>
+            <View style={{width: 58, height: 58, borderWidth: 1,borderColor: '#efefef', borderRadius: 18, justifyContent: 'center', alignItems: 'center'}}>
+              <Image source={props.img} />
+            </View>
+            <Text style={{fontSize: 11, fontWeight: 'bold', textAlign: 'center', marginTop: 6}}>{props.title}</Text>
+        </View>
+    )
+}
+
+export default MainFeature;
+
+const styles = StyleSheet.create({})
