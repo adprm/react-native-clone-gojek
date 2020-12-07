4ad0faa34110652246fcf376b86343056587096f
adprm <aditiyaprmn00@gmail.com>
2020-12-07 19:02:21 +0700

    gopay feature component

--- src/components/moleculs/GopayFeature/index.js
+++ src/components/moleculs/GopayFeature/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 
 const GopayFeature = (props) => {
     return (
-      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-        <Image source={props.img} />
-        <Text style={{fontSize: 13, fontWeight: 'bold', color: 'white', marginTop: 15}}>{props.title}</Text>
-      </View>
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+            <Image source={props.img} />
+            <Text style={{fontSize: 13, fontWeight: 'bold', color: 'white', marginTop: 15, textAlign: 'center'}}>{props.title}</Text>
+        </View>
     )
   }
 
